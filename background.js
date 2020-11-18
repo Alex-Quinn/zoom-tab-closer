dbd5bb190241dd38f6fb53a1a9b2a3fb5f1a4ec6
Alex Quinn <alex@wundercapital.com>
2020-11-18 08:03:22 -0700

    Change timeout

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function closeZoomTabOnSuccessListener(tabId, changeInfo, tab) {
   if (tab.url.match(/https:\/\/(.+\.|)zoom\.us\/.+(\?|\#)success.*/)) {
     setTimeout(() => {
       chrome.tabs.remove(tabId, _ => chrome.runtime.lastError);
-    }, 5000)
+    }, 2000)
   }
 }
 
